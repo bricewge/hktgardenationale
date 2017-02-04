1f1d4805c11b911ea6c8165c492cb50575fe20d8
Brice Waegeneire <brice.wge@gmail.com>
2017-02-04 17:53:45 +0100

    about.html and index.html

--- materialize/js/init.js
+++ materialize/js/init.js
@@ -3,5 +3,22 @@
 
     $('.button-collapse').sideNav();
 
+      $(document).ready(function() {
+          $('select').material_select();
+      });
+
+      $('input.autocomplete').autocomplete({
+          data: {
+              "Chaudronier": null,
+              "Bureau d'étude ": null,
+              "Responsable qualité": null,
+              "Spéciliste cyber-défense": null,
+              "Gardiennage": null,
+              "Mecanien Nucléaire": null,
+              "Logisticien": null
+          },
+          limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+      });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
